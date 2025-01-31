d68e41c17bd4998ceba1da9dacf78a704d15a37a
FlxBot001 <njugunafelix79@gmail.com>
2025-01-31 16:14:43 -0500

    Updated routes for routerdom to route the application click requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-red-500 underline">Hello World</h1>
-      p.
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </Router>
     </>
   )
 }
