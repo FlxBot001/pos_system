2f8a91b3c370a0b6bb5f7cc266447b167f4e9706
FlxBot001 <njugunafelix79@gmail.com>
2025-02-02 15:37:46 -0500

    Updated header section and now functional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaSearch, FaBell} from 'react-icons/fa';
 
 const Header = () => {
   return (
@@ -8,7 +9,31 @@ const Header = () => {
         <img src={logo} alt="restro logo" className="h-8 w-8" />
         <h1 className="text-lg font-semibold text-[#f5f5f5]">Restro</h1>
       </div>
-      
+
+      {/* SEARCH */}
+      <div className="flex items-center gap-4 bg-[#1f1f1f] rounded-[20px] px-5 py-2 w-[500px]">
+        <FaSearch className="text-[#f5f5f5]" />
+        <input
+          type="text"
+          placeholder="Search for food"
+          className="bg-[#1f1f1f] outline-none text-[#f5f5f5]"
+        />
+      </div>
+
+      {/* LOGGED USER DETAILS */}
+      <div>
+        <div>
+          <FaBell className="text-[#f5f5f5] text-2xl" />
+        </div>
+        <div>
+          <FaUserCircle className="text-[#f5f5f5] text-2xl" />
+          <div>
+            <h1>Amrit Raj</h1>
+            <p>Admin</p>
+          </div>
+        </div>
+      </div>
+
     </header>
   )
 }
