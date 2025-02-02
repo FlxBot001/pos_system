66879a16f30eb6560e0376d89c2d3299874703e0
FlxBot001 <njugunafelix79@gmail.com>
2025-02-02 14:13:29 -0500

    Updated nd added pages to the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {HOme, Auth, Orders} from './pages';
 
 function App() {
   const [count, setCount] = useState(0)
