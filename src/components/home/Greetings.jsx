a6ee24b44041b8ac02fd83ece282a7075aaee301
FlxBot001 <njugunafelix79@gmail.com>
2025-02-03 15:14:22 -0500

    Updated files for the Home UI

--- src/components/home/Greetings.jsx
+++ src/components/home/Greetings.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 const Greetings = () => {
-  return (
-    <div>
-        <div className="">
-            <h1>Good Morning, Felix</h1>
-            <p>Give our best services fo our estimed customers</p>
-        </div>
-        <div className="">
-            <h1>time</h1>
-            <p>date</p>
-        </div>
-    </div>
-  )
-}
 
-export default Greetings
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setDateTime(new Date()), 1000);
+    return () => clearInterval(timer)
+  }, []);
+
+  
+
+  return (
+    <div className="flex justify-between items-center px-8 mt-5">
+      <div>
+        <h1 className="text-[#f5f5f5] text-2xl font-semibold tracking-wide">
+          Good Morning, Felix
+        </h1>
+        <p className="text-[#ababab] text-sm">
+          Give our best services fo our estimed customers 😊
+        </p>
+      </div>
+      <div className="">
+        <h1>time</h1>
+        <p>date</p>
+      </div>
+    </div>
+  );
+};
+
+export default Greetings;
