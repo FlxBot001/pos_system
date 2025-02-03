4909dccc5056a38a3ead781ada9afa1f64721479
FlxBot001 <njugunafelix79@gmail.com>
2025-02-03 14:58:53 -0500

    Updated files for home ui

---
+++ src/components/home/Greetings.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Greetings = () => {
+  return (
+    <div>
+        <div className="">
+            <h1>Good Morning, Felix</h1>
+            <p>Give our best services fo our estimed customers</p>
+        </div>
+        <div className="">
+            <h1>time</h1>
+            <p>date</p>
+        </div>
+    </div>
+  )
+}
+
+export default Greetings
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import BottomNav from '../components/BottomNav'
+import Greetings from '../components/home/Greetings'
 
 const Home = () => {
   return (
     <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
       {/* Left Div */}
-      <div className="flex-[3] bg-red-600">
-
+      <div className="flex-[3]">
+        <Greetings />
       </div>
       {/* Right Div */}
       <div className="flex-[2] bg-blue-600">
