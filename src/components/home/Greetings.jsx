d265c3ddef66e0e3fcdf30f16d7a64686c60b05c
FlxBot001 <njugunafelix79@gmail.com>
2025-02-03 15:18:05 -0500

    Updted the sequence for date formats

--- src/components/home/Greetings.jsx
+++ src/components/home/Greetings.jsx
@@ -10,7 +10,23 @@ const Greetings = () => {
     return () => clearInterval(timer)
   }, []);
 
-  
+  const formatDate = (date) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    return `${months[date.getMonth()]} ${String(date.getDate()).padStart(2, '0')}, ${date.getFullYear()}`;
+  }
 
   return (
     <div className="flex justify-between items-center px-8 mt-5">
