b5ff95e405dc07c72c6f853493a1508f6319d907
FlxBot001 <njugunafelix79@gmail.com>
2025-02-03 15:30:04 -0500

    Updated MiniCrd for the HOme UI frontend in my OPS system

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BottomNav from '../components/BottomNav'
 import Greetings from '../components/home/Greetings'
+import MiniCard from '../components/home/MiniCard'
 
 const Home = () => {
   return (
@@ -8,6 +9,10 @@ const Home = () => {
       {/* Left Div */}
       <div className="flex-[3]">
         <Greetings />
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard />
+          <MiniCard />
+        </div>
       </div>
       {/* Right Div */}
       <div className="flex-[2] bg-blue-600">
