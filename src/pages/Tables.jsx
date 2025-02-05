ae2a3c186234e2d269e861de01589f34da1f94a4
FlxBot001 <njugunafelix79@gmail.com>
2025-02-05 06:21:26 -0500

    Updated tables page and now having a back button

--- src/pages/Tables.jsx
+++ src/pages/Tables.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import BottomNav from '../components/shared/BottomNav'
+import BackButton from '../components/shared/BackButton'
 
 const Tables = () => {
   return (
-    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3"'>
+    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden"'>
+      <div className="flex items-center justify-between px-10 py-4">
+        <div className="flex items-center gap-4">
+          <BackButton />
+          <h1 className="text-3xl font-bold text-[#f5f5f5] tracking-wider mt-2">
+            Tables
+          </h1>
+        </div>
+      </div>
+      
       {/* Bottom Nav */}
       <BottomNav />
     </section>
