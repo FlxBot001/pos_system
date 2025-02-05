7e33206ade7eb58fb1590826ac24b774d96f0328
FlxBot001 <njugunafelix79@gmail.com>
2025-02-05 03:36:34 -0500

    Updated OrderCard for the orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import BottomNav from '../components/BottomNav'
+import OrderCard from '../components/order/OrderCard'
 
 const Orders = () => {
   return (
-    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3"'>
+    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden"'>
+      <div className="flex items-center justify-between px-10 py-4">
+        <h1 className="text-2xl font-bold text-[#f5f5f5] tracking-wider mt-2">
+          Orders
+        </h1>
+        <div className="flex items-center justify-around gap-4">
+          <button className="text-lg text-[#ababab] rounded-lg px-5 py-2 font-semibold">
+            All
+          </button>
+          <button className="text-lg text-[#ababab] bg-[#383838] rounded-lg px-5 py-2 font-semibold">
+            In Progress
+          </button>
+          <button className="text-lg text-[#ababab] rounded-lg px-5 py-2 font-semibold">
+            Ready
+          </button>
+          <button className="text-lg text-[#ababab] rounded-lg px-5 py-2 font-semibold">
+            Completed
+          </button>
+
+        </div>
+      </div>
+
+      {/* Order Card */}
+      <OrderCard />
+
       {/* Bottom Nav */}
       <BottomNav />
     </section>
