dcb2e4f79924b9e9667cfa0cea20f61426d6ed14
FlxBot001 <njugunafelix79@gmail.com>
2025-02-05 01:02:55 -0500

    Created a left UI for home and replaced with popular shishes

---
+++ src/components/home/PopularDishes.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const PopularDishes = () => {
+    return (
+        <div className="mt-6 pr-6">
+            <div className="w-full bg-[#1a1a1a] rounded-lg">
+                <div className="flex justify-between items-center px-6 py-4">
+                    <h1 className="text-[#f5f5f5] text-lg font-semibold">
+                        Popular Dishes
+                    </h1>
+                    <a href="" className="text-[#025cca] text-sm font-semibold">
+                        View all
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PopularDishes
