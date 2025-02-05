c1e5829ebc506c7fb673db121596ca9d11e9316c
FlxBot001 <njugunafelix79@gmail.com>
2025-02-05 15:40:58 -0500

    Updated Modal for the bottom nav component

--- src/components/shared/BottomNav.jsx
+++ src/components/shared/BottomNav.jsx
@@ -14,23 +14,23 @@ const BottomNav = () => {
     const navigate = useNavigate();
     return (
         <div className="fixed bottom-0 left-0 right-0 p-2 flex justify-around bg-[#262626] h-15">
-            <button 
+            <button
                 onClick={() => navigate("/")}
-            className='flex items-center justify-center cursor-pointer text-[#f5f5f5] w-[200px] bg-[#343434] rounded-[20px]'
+                className='flex items-center justify-center cursor-pointer text-[#f5f5f5] w-[200px] bg-[#343434] rounded-[20px]'
             >
                 <FaHome className='inline mr-4 size={20}}' />
                 <p>Home</p>
             </button>
-            <button 
-            onClick={() => navigate("/orders")}
-            className='flex items-center justify-center cursor-pointer text-[#f5f5f5] w-[200px]'
+            <button
+                onClick={() => navigate("/orders")}
+                className='flex items-center justify-center cursor-pointer text-[#f5f5f5] w-[200px]'
             >
                 <MdOutlineReorder className='inline mr-4 size={20}' />
                 <p>Orders</p>
             </button>
-            <button 
-            onClick={() => navigate("/tables")}
-            className='flex items-center justify-center cursor-pointer text-[#f5f5f5] w-[200px]'>
+            <button
+                onClick={() => navigate("/tables")}
+                className='flex items-center justify-center cursor-pointer text-[#f5f5f5] w-[200px]'>
                 <MdTableBar className='inline mr-4 size={20}' />
                 <p>Tables</p>
             </button>
@@ -39,8 +39,11 @@ const BottomNav = () => {
                 <p>More</p>
             </button>
 
-            <button className='cursor-pointer absolute bottom-5 bg-[#F6B100] text-[#f5f5f5] rounded-full p-3 items-center'>
-                <BiSolidDish size={30}/>
+            <button
+                onClick={openModal}
+                className='cursor-pointer absolute bottom-5 bg-[#F6B100] text-[#f5f5f5] rounded-full p-3 items-center'
+            >
+                <BiSolidDish size={30} />
             </button>
         </div>
     )
