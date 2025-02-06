1f3ede4a707e0b68dc480911fcba495ca968e65f
FlxBot001 <njugunafelix79@gmail.com>
2025-02-06 09:01:58 -0500

    Updated Customer Info

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -39,10 +39,27 @@ const Menu = () => {
       </div>
 
       {/* Right Div */}
-      <div className="flex-[1] bg-blue-500 h-full">
+      <div className="flex-[1] bg-[#1a1a1a] mt-4 mr-3 h-[780px] rounded-lg pt-2">
         {/* Customer INfo */}
-
+        <div className="flex items-center justify-between px-4 py-3">
+          <div className="flex flex-col items-start">
+            <h1 className="text-md font-semibold tracking-wide text-[#f5f5f5]">
+              Customer Name
+            </h1>
+            <p className="text-xs font-medium mt-1 text-[#ababab]">
+              #101/Dine in
+            </p>
+            <p className="text-xs font-medium mt-2 text-[#ababab]">
+              January 19, 2025 05:34 PM
+            </p>
+          </div>
+          <button className="text-xl font-bold rounded-lg bg-[#f6b100]">
+            CN
+          </button>
+        </div>
+        
         {/* Cart Items */}
+        .
 
         {/* Bills */}
       </div>
