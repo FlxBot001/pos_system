81fff341244391bd8c6cc1248a1e8c0e1a949346
FlxBot001 <njugunafelix79@gmail.com>
2025-02-06 07:23:38 -0500

    Updated menu page to contain selected items

--- src/components/menu/MenuContainer.jsx
+++ src/components/menu/MenuContainer.jsx
@@ -38,6 +38,30 @@ const MenuContainer = () => {
                 )
             })}
         </div>
+
+        {/* Border */}
+        <hr className="border-t-2 mt-4 border-[#2a2a2a]" />
+
+        <div className="grid grid-cols-4 gap-4 px-10 py-4 w-[100%]">
+            {/* Menu Category map */}
+            {selected?.items.map((menu,) => {
+                return (
+                    <div 
+                    key={menu.id}
+                    className="flex flex-col items-start bg-[#1a1a1a] hover:bg-[#2a2a2a] justify-between p-4 rounded-lg cursor-pointer h-[150px]"
+                    >
+                        <div className="flex items-center justify-between w-full">
+                            <h1 className="text-lg font-semibold text-[#f5f5f5]">
+                                {menu.name}
+                            </h1>
+                        </div>
+                        <p className="text-xl text-[#ababab] font-bold">
+                            {menu.price}
+                        </p>
+                    </div>
+                )
+            })}
+        </div>
     </>
   )
 }
