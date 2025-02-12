dc67917fc1409c6cd8edb87f9003f0d8ea06c16b
FlxBot001 <njugunafelix79@gmail.com>
2025-02-12 11:33:41 -0500

    Updated app.jsx file for authpage router

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-undef */
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import {Home, Auth, Orders, Menu, Rooms, More} from './pages';
 import Header from './components/shared/Header';
 import Tables from './pages/Tables';
 
-function App() {
+function Layout() {
+  const location = useLocation();
+  const hideHeaderRoutes = ["/auth"];
   
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/tables" element={<Tables />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
-          <Route path="/rooms" element={<Rooms />} />
-          <Route path="/more" element={<More />} />
-        </Routes>
-      </Router>
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/tables" element={<Tables />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/more" element={<More />} />
+      </Routes>
     </>
   )
+}
+
+function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
+  )  
 }
 
 export default App
