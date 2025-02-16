ababc2af9642cdc372c268e3835a643130ba9acd
FlxBot001 <njugunafelix79@gmail.com>
2025-02-16 16:32:57 -0500

    Updated sidebar

--- src/routes/layout.jsx
+++ src/routes/layout.jsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { Sidebar } from '../layouts/Sidebar';
 
 const Layout = () => {
   return (
     <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-900">
-
+        <Sidebar />
     </div>
   )
 }
