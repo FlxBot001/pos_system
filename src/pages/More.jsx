55ef9fcc28929b474e1d7ab3430bfc5bd75202cd
FlxBot001 <njugunafelix79@gmail.com>
2025-02-16 14:38:00 -0500

    Updated More pages

--- src/pages/More.jsx
+++ src/pages/More.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import BottomNav from '../components/shared/BottomNav'
 // import MostOrdered from './MostOrdered';
 // import MostTypeOfOrder from './MostTypeOfOrder';
 // import StatsCard from './StatsCard';
@@ -10,7 +11,8 @@ function More() {
 
   return (
     <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
-
+      {/* Bottom Nav */}
+      <BottomNav />
     </section>
   )
 }
