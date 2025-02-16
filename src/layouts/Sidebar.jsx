83e026eee80becd1652d3d8875478e0ec2737ad0
FlxBot001 <njugunafelix79@gmail.com>
2025-02-16 17:45:11 -0500

    Updated sidebar component

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,8 +1,23 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { forwardRef } from 'react'
+import { cn } from '../utils/cn';
+import logo from "../assets/logo1.png"
 
-export const Sidebar = () => {
+const Sidebar = forwardRef(({}, ref) => {
   return (
-    <aside>Sidebar</aside>
-  )
-}
+    <aside 
+      ref={ref}
+      className={cn(
+        "fixed z-[100] flex h-full w-[240px] flex-col overflow-x-hidden border-r border-slate-300 bg-white [transition:_300ms_cubic_bezier(0.4,_0,_0.2,_1),_left_300ms_cubic-bezier(0.4,_0,_0.2,_1),_background-color_150ms_cubic-bezier(0.4,_0,_0.2,_1),_border_150ms_cubic-bezier(0.4,_0,_0.2,_1)] dark:border-slate-700 dark:bg-slate-900"
+      )}
+    >
+      <div className="flex gap-x-3 p-3">
+        <img src={logo} alt="forkly" />
+      </div>
+    </aside>
+  );
+});
+
+Sidebar.displayName = 'Sidebar';
+
+// export { Sidebar };
--- src/routes/layout.jsx
+++ src/routes/layout.jsx
@@ -1,8 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+
+import { Outlet } from "react-router-dom";
+
 import { Sidebar } from '../layouts/Sidebar';
-import { cn } from "../utils/cn";
 import { Header } from '../layouts/Header';
+
+import { cn } from "../utils/cn";
 
 const Layout = () => {
   return (
@@ -12,6 +16,9 @@ const Layout = () => {
             "transition-[margin] duration-300"
         )}>
             <Header />
+            <div className="overflow-y-auto h-[calc(100vh-60px)] p-6 overflow-x-hidden">
+                <Outlet />
+            </div>
         </div>
     </div>
   )
