2370270db3b5200fa0a48413c8251acd017371b7
FlxBot001 <njugunafelix79@gmail.com>
2025-02-16 16:38:59 -0500

    Updated header component

--- src/routes/layout.jsx
+++ src/routes/layout.jsx
@@ -1,11 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { Sidebar } from '../layouts/Sidebar';
+import { cn } from "../utils/cn";
+import { Header } from '../layouts/Header';
 
 const Layout = () => {
   return (
     <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-900">
         <Sidebar />
+        <div className={cn(
+            "transition-[margin] duration-300"
+        )}>
+            <Header />
+        </div>
     </div>
   )
 }
